5faeab636dd4d4b8b9cd39a5658a084f486dd45a
daencer <33512814+daencer@users.noreply.github.com>
2017-11-14 16:04:20 +0100

    Test

--- dice_game_v4.py
+++ dice_game_v4.py
@@ -1,40 +1,3 @@
-from random
-# Function: Roll Dice
-def roll_dice(number = 1, faces = 6, seed = None):
-    result = ''
-    random.seed(seed)
-    for i in range(number):
-            result += str(random.randint(1, faces))
-            if i + 1 < number:
-                result += ','
-    return result
-
-
-def players():
-    while True:
-        try:
-            players = int(input("Geben Sie ihre Spieleranzahl ein: "))
-            if players == 'stop':
-                quit()
-                # else:
-                # players_new = input("Bitte eine Zahl eingeben: ")
-            break
-        except(ValueError, IndexError):
-            print("Try again must be a number!")
-    return players
-
-member = []
-
-counter_player = players()
-count = 1
-while count <= counter_player:
-    player_name = input("Name eingeben: ")
-    member.append(player_name)
-    count += 1
-print(member)
-toss_1 = roll_dice()
-print(toss_1)
-
 """Docstring: A very short sentence explaining the function. < 79 characters.
 
 additional informaion if required
@@ -43,7 +6,7 @@ and more infos
 """
 
 import random
-
+from time import sleep
 # from numpy import array  #  an aother example
 # from meinmodul import test  #  example for your own module
 
@@ -52,6 +15,8 @@ __copyright__ = "Copyright 2017/2018 – EPR-Goethe-Uni"
 __credits__ = "If you would like to thank somebody \
 i.e. an other student for his code"
 __email__ = "s1539940@stud.uni-frankfurt.de"
+# Here are all lists defined:
+list_amount_players = []
 
 # Function: Checks if a number is in specific interval
 def in_interval(number, start, stop):
@@ -79,8 +44,10 @@ def roll_cheating_dice(seed = None):
 # Function: Game
 def sixteen_is_dead(players):
     print(None)
+
+
 # Function: amount of Players
-def players():
+def amount_players():
     while True:
         try:
             players = int(input("Geben Sie ihre Spieleranzahl ein: "))
@@ -95,16 +62,35 @@ def players():
 def name_players():
     member = []
 
-    counter_player = players()
+    counter_player = amount_players()
     count = 1
     while count <= counter_player:
         player_name = input("Name eingeben: ")
         member.append(player_name)
         count += 1
-
+        print(member)
 
 
 # ~~~~~~~~~~~~~~~~ Main Game Frame ~~~~~~~~~~~~~~~~~~~~~
 
 print("########################################")
 print("# [1] Play the Dice Game               #")
+print("# [2] Settings                         #")
+print("# [3] Credits                          #")
+print("#                                      #")
+print("# What is the correct answer?....      #")
+print("########################################")
+
+
+while True:
+    try:
+        console_input = int(input("Choose wisely: "))
+        if console_input == 1:
+            print("Lets start the Game!")
+            amount_players()
+            name_players()
+        break
+    except(ValueError, IndexError):
+        print("Try again must be a number!")
+
+
