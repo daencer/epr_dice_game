f5e0b143cd3575383a5328b5b1090064488f78ab
Markus <mkalusche@me.com>
2017-11-15 17:47:58 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	dice_game_v4.py

--- dice_game_v5.py
+++ dice_game_v5.py
@@ -37,8 +37,33 @@ def sum_from_string(s):
         total += int(i)
     return total
 
+# Function: amount of Players
+def amount_players():
+    while True:
+        try:
+            players = int(input('Geben Sie ihre Spieleranzahl ein: '))
+            if players == 'stop':
+                quit()
+            break
+        except(ValueError, IndexError):
+            print('Try again must be a number!')
+    return players
+
+# Function: Name the players
+def name_players():
+    member = []
+
+    counter_player = amount_players()
+    count = 1
+    while count <= counter_player:
+        player_name = input('Name von Spieler ' + str(count) + ' eingeben: ')
+        member.append(player_name)
+        count += 1
+        #print(member)
+    return member
+
 # Function: Game
-def sixteen_is_dead(players):
+def sixteen_is_dead(players, members):
     loser = []
     lowest_result = 0
     hit_sixteen = False
@@ -48,7 +73,7 @@ def sixteen_is_dead(players):
         if continue_game:
             total = 0
             last_turn = False
-            print('Spieler', i)
+            print(members[i])
             # roll the dice(s) without end
             while True:
                 cin = input('Um zu wuerfeln, druecke ENTER... ')
@@ -75,7 +100,7 @@ def sixteen_is_dead(players):
                     last_turn = True
                 # player reaches the total of 16 or higher
                 if total >= 16:
-                    loser = [i]
+                    loser = [members[i]]
                     hit_sixteen = True
                 # stop if player hit sixteen or his last turn
                 if hit_sixteen or last_turn:
@@ -85,10 +110,10 @@ def sixteen_is_dead(players):
                 lowest_result = total
             # add new loser to others
             if total == lowest_result:
-                loser.append(i)
+                loser.append(members[i])
             # define new loser
             if total < lowest_result:
-                loser = [i]
+                loser = [members[i]]
                 lowest_result = total
             #print('schlechteste aktuell:', loser, 'mit', lowest_result)
             if hit_sixteen:
@@ -99,6 +124,72 @@ def sixteen_is_dead(players):
     if hit_sixteen:
         print('Die 16 wurde getroffen.')
     else:
-        print('Verlierer ist/sind', loser, 'mit nur', lowest_result)
+        print('Verlierer ist/sind')
+        for i in loser:
+            print(i)
+        print('mit nur', lowest_result, 'Summenpunkten')
 
-sixteen_is_dead(9)
+# ~~~~~~~~~~~~~~~~ Main Game Frame ~~~~~~~~~~~~~~~~~~~~~
+
+print("########################################")
+print("# [1] Play the Dice Game               #")
+print("# [2] Settings                         #")
+print("# [3] Credits                          #")
+print("#                                      #")
+print("# What is the correct answer?....      #")
+print("########################################")
+
+while True:
+    try:
+        cin = int(input("Choose wisely: "))
+        print( "\n")
+        if cin == 1:
+            member_list = name_players()
+            sixteen_is_dead(len(member_list), member_list)
+        """
+            while count <= len(member_list) - 1:
+                try:
+                    print("\n")
+                    print(member_list[count],"ist am Zug!")
+                    user_input = input("Würfeln 'Enter', Aufhören 'n': ")
+                    sleep(3)
+                    if user_input == "":
+                        x = roll_dice()
+                        score.append(int(x))
+                        z = sum(score)
+                        print("Gewürfelt:   " + x )
+                        print("Insgesamt:   " + str(z))
+    #AB HIER FUNKTIONIERT NOCH NICHTS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#                    elif sum(score) == 10:
+ #                       user_input = input("Sie müssen nochmal würfeln!")
+  #                  elif user_input == "stop":
+   #                     print(z)
+    #                    quit()
+     #               elif sum(score) >= 16 or z == 9:
+      #                  print("Sie haben verloren! try again!")
+       #                 break
+        #            elif user_input == "n":
+         #               count += 1
+          #              score.append(sum(score))
+           #             score = []
+            #        else:
+             #           print(player_1, "Ihr Ergebnis lautet: ", z)
+              #          if z >= 16 or z == 9:
+               #             print("Sie haben verloren")
+                #            print(toss_2)
+                 #           break
+                  #      else:
+                   #         print(toss_2)
+                    #        break
+                except(IndexError, ValueError):
+                    print("Only 'Enter' or 'n'")
+#BIS HIER ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        elif cin == 2:
+            print("Settings")
+        elif cin == 3:
+            print("Credits")
+        elif cin == 42:
+            print("cheat Menu")"""
+        break
+    except(IndexError, ValueError):
+        print("Only numbers!")
